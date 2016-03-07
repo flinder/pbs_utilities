0ee5bb0f0d433912fdb0d5c46a815d87359ea81a
flinder <fridolin.linder@gmail.com>
2016-03-06 21:17:06 -0500

    added some documentation

--- queue.py
+++ queue.py
@@ -9,6 +9,22 @@ import datetime
 
 
 class PBSQueue(object):
+    '''
+    PBS personal queue class
+
+    Monitors a users pbs job submissions. Given a reservoir of jobs and the 
+    desired number of parallel jobs, the queue submits new jobs as soon as the
+    number of jobs drops below the desired number.
+
+    Arguments:
+    ----------
+    user_id: str, pbs user id.
+    reservior: str, path to folder containing the pbs job files.
+    num_jobs: int, how many jobs sould be running in parallel.
+    job_regex: str, which files in reservior should be used (can use glob
+        compatible wildcards.
+    remove_submitted: bool, should the job files be deleted after submission.
+    '''
 
     def __init__(self, user_id, reservoir, num_jobs, job_regex=r'*', remove_submitted=True):
         self.split_regex = re.compile(r'\s+')
@@ -17,7 +33,7 @@ class PBSQueue(object):
         self.running_jobs = 0
 	self.user_id = user_id
         
-        # Get the job file reservior
+        # TODO get this runnign with variable input path
         job_files = glob.glob('../bruce_shared/text_reuse/generate_alignments/pbs_scripts/*')
         self.queue = sorted(job_files)
 	print len(self.queue)
@@ -26,6 +42,18 @@ class PBSQueue(object):
 	self.remove_submitted=remove_submitted
 
     def update(self):        
+        '''
+        Update status of the queue.
+
+        Checks the status of the users jobs in the pbs system.
+
+        Arguments:
+        ----------
+        
+        Returns:
+        ---------
+        Updates the running_jobs attribute of the queue
+        '''
         # Request status through shell
         response = subprocess.check_output(['qstat', '-u', self.user_id])
         # Get the parsed job list
@@ -33,6 +61,17 @@ class PBSQueue(object):
         self.running_jobs = sum(j['status'] in ['R', 'Q'] for j in jobs)
 
     def _submit_job(self, job_file): 
+        '''
+        Submit a job to the pbs queue.
+
+        Arguments:
+        ---------
+        job_file: str, path to the file to be submitted
+
+        Returns:
+        ---------
+        None
+        '''
         subprocess.check_output(['qsub', job_file])
 	print "submitting {}".format(job_file)
 	self.submitted_jobs.append(job_file)
@@ -40,6 +79,17 @@ class PBSQueue(object):
 	    os.remove(job_file)
 
     def submit_jobs(self):
+        '''
+        Submit jobs until the desired amount of jobs is runnig
+
+        Arguments:
+        ---------
+        None
+
+        Returns:
+        ----------
+        None
+        '''
         self.last_difference = self.num_jobs - self.running_jobs
         c = 1
         while c <= self.last_difference:
@@ -50,6 +100,18 @@ class PBSQueue(object):
             self.queue.pop(0)
 
     def _parse_output(self, output):
+        '''
+        Parse output from qstat pbs commandline program
+        
+        Arguments:
+        ----------
+        output: str, output obtained from qstat command
+
+        Returns:
+        ---------
+        list, of all jobs. Each job is represented as a dictionary conainting
+            all relevant information.
+        '''
         lines = output.split('\n')
         del lines[:5]
         jobs = [] 
